2ae01a4d0c864d105e331f653bb93ba65697f76e
Aribela <jair.castaneda@framelova.com>
2019-07-09 11:38:46 -0500

    Update NetDevices.java

--- www/NetDevices.java
+++ www/NetDevices.java
@@ -27,43 +27,32 @@ public class NetDevices  extends CordovaPlugin {
         return false;
     }
     private void obtenerips(String msg, CallbackContext callbackContext){
-        //FileWriter fichero=new FileWriter("/Users/ari/Desktop/f.txt");
-        //BufferedWriter bfwriter = new BufferedWriter(fichero);
-        String todo = "";
+        callbackContext.success("Hola");
+        // String todo = "";
 
-        try {
-            Process proc = Runtime.getRuntime().exec("arp -a ");
+        // try {
+        //     Process proc = Runtime.getRuntime().exec("arp -a ");
 
-            BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-            BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+        //     BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        //     BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
 
-            // read the output from the command
-            String s = null;
+        //     // read the output from the command
+        //     String s = null;
             
 
-            while ((s = stdInput.readLine()) != null) {
-                String[] array = s.split("\\u0020"); //split que separa el arreglo
-                String ar1=array[1]; //contiene la posicion 1 del arreglo (donde se encuentra la IP)
-                String ar2=array[3];// contiene la posicion 3 del arreglo (donde se encuentra la MAC Address)
-                String linea=array[1]+";"+array[3];
-                todo = todo+"|"+linea;
-                //fichero.write(todo);
-                //bfwriter.newLine();
-
-                //fichero.append("\r\n");
-
-                // read any errors from the attempted command
-                //while ((s = stdError.readLine()) != null) {
-                    //System.err.println(s);
-                //}
-            }
-            callbackContext.success(todo);
-        }
-        catch (IOException ex) {
-            //System.err.println(ex);
-            callbackContext.error("Error");
-        } 
-        //fichero.close();  
+        //     while ((s = stdInput.readLine()) != null) {
+        //         String[] array = s.split("\\u0020"); //split que separa el arreglo
+        //         String ar1=array[1]; //contiene la posicion 1 del arreglo (donde se encuentra la IP)
+        //         String ar2=array[3];// contiene la posicion 3 del arreglo (donde se encuentra la MAC Address)
+        //         String linea=array[1]+";"+array[3];
+        //         todo = todo+"|"+linea;
+        //     }
+        //     callbackContext.success(todo);
+        // }
+        // catch (IOException ex) {
+        //     //System.err.println(ex);
+        //     callbackContext.error("Error");
+        // } 
     } 
 
 }
