bac3030070ac0bda5f54182674dac995f703c4c7
Aribela <jair.castaneda@framelova.com>
2019-07-09 12:44:42 -0500

    Update NetDevices.java

--- www/NetDevices.java
+++ www/NetDevices.java
@@ -20,44 +20,44 @@ public class NetDevices  extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
-            // if ("obtenerips".equals(action)) {
-            //     obtenerips(args.getString(0), callbackContext);
-            //     return true;
-            // }
-            // else {
+            if ("obtenerips".equals(action)) {
+                obtenerips(args.getString(0), callbackContext);
+                return true;
+            }
+            else {
                 callbackContext.error("Incorrect action parameter: " + action);
                 return false;
-            // }
+            }
 
     }
     private void obtenerips(String msg, CallbackContext callbackContext){
-        callbackContext.success("Hola");
-        // String todo = "";
+        // callbackContext.success("Hola");
+        String todo = "";
 
-        // try {
-        //     Process proc = Runtime.getRuntime().exec("arp -a ");
+        try {
+            Process proc = Runtime.getRuntime().exec("arp -a ");
 
-        //     BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-        //     BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
 
-        //     // read the output from the command
-        //     String s = null;
+            // read the output from the command
+            String s = null;
             
 
-        //     while ((s = stdInput.readLine()) != null) {
-        //         String[] array = s.split("\\u0020"); //split que separa el arreglo
-        //         String ar1=array[1]; //contiene la posicion 1 del arreglo (donde se encuentra la IP)
-        //         String ar2=array[3];// contiene la posicion 3 del arreglo (donde se encuentra la MAC Address)
-        //         String linea=array[1]+";"+array[3];
-        //         todo = todo+"|"+linea;
-        //     }
-        //     callbackContext.success(todo);
-        // }
-        // catch (IOException ex) {
-        //     //System.err.println(ex);
-        //     callbackContext.error(ex.getMessage());
+            while ((s = stdInput.readLine()) != null) {
+                String[] array = s.split("\\u0020"); //split que separa el arreglo
+                String ar1=array[1]; //contiene la posicion 1 del arreglo (donde se encuentra la IP)
+                String ar2=array[3];// contiene la posicion 3 del arreglo (donde se encuentra la MAC Address)
+                String linea=array[1]+";"+array[3];
+                todo = todo+"|"+linea;
+            }
+            callbackContext.success(todo);
+        }
+        catch (IOException ex) {
+            //System.err.println(ex);
+            callbackContext.error(ex.getMessage());
 
-        // } 
+        } 
     } 
 
 }
