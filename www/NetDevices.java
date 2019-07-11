3b3781625cea1ceb4b2409f55a5f4b1ba21b370c
Aribela <jair.castaneda@framelova.com>
2019-07-11 11:07:22 -0500

    Update NetDevices.java

--- www/NetDevices.java
+++ www/NetDevices.java
@@ -57,15 +57,15 @@ public class NetDevices  extends CordovaPlugin {
             while ((s = stdInput.readLine()) != null) {
                 if(cont > 1){
                     // String[] array = s.split("\\u0020"); //split que separa el arreglo
-                    String[] array = s.split("\t"); //split que separa el arreglo
+                    String[] array = s.split("\\t"); //split que separa el arreglo
                     String ar1=array[0]; //contiene la posicion 1 del arreglo (donde se encuentra la IP)
                     String ar2=array[2];// contiene la posicion 3 del arreglo (donde se encuentra la MAC Address)
                     // String ar2=array[4];
                     // String ar2=array[5];
-                    if(ar2 != "00:00:00:00:00:00"){
+                    // if(ar2 != "00:00:00:00:00:00"){
                         String linea=array[0]+";"+array[2]+";"+array[3]+";"+array[4];
                         todo = todo+"|"+linea;
-                    }
+                    // }
                 }
                 cont++;
             }
